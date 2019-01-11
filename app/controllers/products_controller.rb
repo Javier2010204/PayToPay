e3d562e3944f63c86a8c5e9eace9c9734112c7c7
Javier2010204 <2010204javiergomez@gmail.com>
2019-01-11 12:47:58 -0600

    Agregar avatar a productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -54,7 +54,7 @@ class ProductsController < ApplicationController
 	private
 
 		def product_params
-			params.require(:product).permit(:name, :description, :pricing)
+			params.require(:product).permit(:name, :description, :pricing, :avatar)
 		end
 
 		def set_product
