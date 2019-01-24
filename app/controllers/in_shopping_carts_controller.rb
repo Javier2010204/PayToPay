f623c7135387aaf383a4235a5faaa19d5d6aa012
Javier2010204 <2010204javiergomez@gmail.com>
2019-01-23 18:41:59 -0600

    error en form

--- app/controllers/in_shopping_carts_controller.rb
+++ app/controllers/in_shopping_carts_controller.rb
@@ -2,7 +2,7 @@ class InShoppingCartsController < ApplicationController
 
     def create
         in_shopping_cart = InShoppingCart.new(product_id: params[:product_id], shopping_cart: @shopping_carts)
-
+        
         if in_shopping_cart.save
             redirect_to carrito_path, notice: "Guardamos el producto en el carrito" 
         else
